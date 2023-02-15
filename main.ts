c4dc9a083f683c8c23ad554602415d8c85373bf3
David K <david.kennedy@mailfence.com>
2023-02-15 22:29:27 +0000

    Added a function to check space and a timeout to the pulse read

--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
-let distance = 0
-basic.forever(function () {
-    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 59)
-    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 59)
-    pins.digitalWritePin(DigitalPin.P10, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P10, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P10, 0)
-    distance = Math.idiv(pins.pulseIn(DigitalPin.P1, PulseValue.High), 58)
-    if (distance < 50) {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 30)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 30)
-        basic.pause(500)
+function thereIsSpace () {
+    for (let index = 0; index < 5; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        duration = pins.pulseIn(DigitalPin.P2, PulseValue.High, 40000)
+if (duration != 0 && duration > 38000 || Math.idiv(duration, 58) > 20) {
+            return 1
+        }
     }
-    basic.pause(50)
+    return 0
+}
+let distance = 0
+let duration = 0
+basic.forever(function () {
+    led.enable(false)
+    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 59)
+    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 59)
+    basic.pause(100)
+    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+    distance = 10
+    while (thereIsSpace() == 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
+        basic.pause(100)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+    }
 })
