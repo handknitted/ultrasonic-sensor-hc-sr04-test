dd2e82781fc30f046ec947e30de653797a0f0521
David K <david.kennedy@mailfence.com>
2023-02-15 22:47:57 +0000

    Decreased sample size and changed motor speed

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 function thereIsSpace () {
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 3; index++) {
         pins.digitalWritePin(DigitalPin.P1, 0)
         control.waitMicros(10)
         pins.digitalWritePin(DigitalPin.P1, 1)
@@ -12,20 +12,16 @@ if (duration != 0 && duration > 38000 || Math.idiv(duration, 58) > 20) {
     }
     return 0
 }
-let distance = 0
 let duration = 0
 basic.forever(function () {
     led.enable(false)
-    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 59)
-    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 59)
+    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
+    kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
     basic.pause(100)
-    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-    kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-    distance = 10
     while (thereIsSpace() == 0) {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 50)
-        basic.pause(100)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 90)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 90)
+        basic.pause(200)
         kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
         kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
     }
